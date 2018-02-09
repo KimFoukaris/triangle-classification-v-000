181090170854a17637bcb4e37a7e95bcd43be01f
Kim Foukaris <kimfoukaris@outlook.com>
2018-02-09 11:37:13 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,16 +8,24 @@ class Triangle
   end
 
   def kind
-    if side1 == side2 && side1 == side3
+    if 
+      side1 == side2 && side1 == side3
       type = :equilateral
       #type
     elsif side1 == side2 || side1 == side3 || side2 == side3
       type = :isosceles
       #type
-    else
+    elsif
       type = :scalene
-
+    else
+      if side1 == 0 || side2 == 0 || side 3 == 0
+        raise IllegalError
+      
+    elsif
     end
   end
+
+class IllegalError < StandardError
+end
 
 end
