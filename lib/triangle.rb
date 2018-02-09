30b6e3045665c1232c853af9b2e032d4fba873e5
Kim Foukaris <kimfoukaris@outlook.com>
2018-02-09 11:27:35 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,8 +11,7 @@ class Triangle
     if side1 == side2 && side1 == side3
       type = :equilateral
       #type
-    end
-    if side1 == side2 || side1 == side3 || side2 == side3
+    elsif side1 == side2 || side1 == side3 || side2 == side3
       type = :isosceles
       #type
     end
