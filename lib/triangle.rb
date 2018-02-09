62310b02615f30536c300f8ff7bdc26db25e71a2
Kim Foukaris <kimfoukaris@outlook.com>
2018-02-09 11:21:27 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,4 +6,12 @@ class Triangle
     @side2 = side2
     @side3 = side3
   end
+
+  def kind(side1, side2, side3)
+    if side1 == side2 == side3
+      type = :equilateral
+      type
+    end
+  end
+  
 end
