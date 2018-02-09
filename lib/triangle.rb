f006cbb712f748bd5bd40700d9a9d4db84bb490a
Kim Foukaris <kimfoukaris@outlook.com>
2018-02-09 12:38:18 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,8 @@ class Triangle
 
   def kind
     if
-      side1 == 0 || side2 == 0 || side3 == 0
+      (side1 == 0 || side2 == 0 || side3 == 0) ||
+      (side1 < 0 || side2 < 0 || side3 <0 )
         raise TriangleError
     elsif
       side1 == side2 && side1 == side3
